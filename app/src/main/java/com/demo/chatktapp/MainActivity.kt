21250c542ab3a4712ea4cfc9fa924329f586be82
Asa <asareel56@gmail.com>
2022-04-13 22:56:28 +0200

    added layout for username input page

--- app/src/main/java/com/demo/chatktapp/MainActivity.kt
+++ app/src/main/java/com/demo/chatktapp/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.demo.chatktapp
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var sharedEditor: SharedPreferences.Editor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        /**
+         * Determine if the app was opened for the first time
+         */
+        sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+        sharedEditor = sharedPreferences.edit()
+
+        when(isFirstTime()){
+            true -> {
+                setContentView(R.layout.fragment_welcome)
+            }
+            false -> {
+                setContentView(R.layout.activity_main)
+            }
+        }
+    }
+
+    private fun isFirstTime(): Boolean {
+        if (sharedPreferences.getBoolean("firstTime", true)){
+            sharedEditor.putBoolean("firstTime", false)
+            sharedEditor.commit()
+            sharedEditor.apply()
+            return true
+        }
+        return false
     }
 }
